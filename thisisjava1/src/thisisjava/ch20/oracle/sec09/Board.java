67d8c8f7eea63b07fcd4d4d70dda60b7142bd2fd
guswo4324 <phj2394324@naver.com>
2025-05-28 03:06:07 +0900

    create branch test

--- thisisjava1/src/thisisjava/ch20/oracle/sec09/Board.java
+++ thisisjava1/src/thisisjava/ch20/oracle/sec09/Board.java
@@ -6,6 +6,7 @@ import java.sql.Blob;
 
 @Data
 public class Board {
+	
 	private int bno;
 	private String btitle;
 	private String bcontent;
@@ -13,4 +14,48 @@ public class Board {
 	private Date bdate;
 	private String bfilename;
 	private Blob bfiledata;
+	
+	public int getBno() {
+		return bno;
+	}
+	public void setBno(int bno) {
+		this.bno = bno;
+	}
+	public String getBtitle() {
+		return btitle;
+	}
+	public void setBtitle(String btitle) {
+		this.btitle = btitle;
+	}
+	public String getBcontent() {
+		return bcontent;
+	}
+	public void setBcontent(String bcontent) {
+		this.bcontent = bcontent;
+	}
+	public String getBwriter() {
+		return bwriter;
+	}
+	public void setBwriter(String bwriter) {
+		this.bwriter = bwriter;
+	}
+	public Date getBdate() {
+		return bdate;
+	}
+	public void setBdate(Date bdate) {
+		this.bdate = bdate;
+	}
+	public String getBfilename() {
+		return bfilename;
+	}
+	public void setBfilename(String bfilename) {
+		this.bfilename = bfilename;
+	}
+	public Blob getBfiledata() {
+		return bfiledata;
+	}
+	public void setBfiledata(Blob bfiledata) {
+		this.bfiledata = bfiledata;
+	}
+
 }
--- thisisjava1/src/thisisjava/ch20/oracle/sec09/User.java
+++ thisisjava1/src/thisisjava/ch20/oracle/sec09/User.java
@@ -4,9 +4,42 @@ import lombok.Data;
 
 @Data //Constructor, Getter, Setter, hashCode(), equals(), toString() 자동생성
 public class User {
+	
 	private String userId;
 	private String userName;
 	private String userPassword;
 	private int userAge;
 	private String userEmail;
+	
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public int getUserAge() {
+		return userAge;
+	}
+	public void setUserAge(int userAge) {
+		this.userAge = userAge;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+
 }
